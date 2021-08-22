f64ce1e9e36ea4b3fa0bafffb208a962f67654a1
Chesebro11 <84700256+Chesebro11@users.noreply.github.com>
2021-08-22 16:03:50 -0700

    finished up the function that selects parameters for the password. started to create random selector

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,13 @@
 // Assignment code here
 // Arrays:
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "/", ">", "?", "<", "-", "=", "+", "~"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "/", ">", "?", "<", "-", "=", "+", "~"];
 var lowerAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Input Variables
 var confirmLength;
+
 var confirmSpecial;
 var confirmNumber;
 var confirmLowerAlpha;
@@ -30,15 +31,67 @@ function generatePassword() {
         confirmNumber = window.confirm("Would you like to include numbers?");
         confirmSpecial = window.confirm("Would you like to include special characters?");
 
-        // if statement if they choose no parameters
+        // if/else statement for determining which parameters were selected
         if (confirmLowerAlpha === false && confirmUpperAlpha === false && confirmNumber === false && confirmSpecial === false) {
             window.alert("You must select at least one parameter");
             parameterConfirm();
         }
-        // else to move on through the function
-        else {}
-    }
-}
+        // else if for all options
+        else if (confirmLowerAlpha && confirmUpperAlpha && confirmNumber && confirmSpecial) {
+            parameters = lowerAlpha + upperAlpha + number + specialChar
+        }
+        // else ifs for only 3 options
+        else if (confirmLowerAlpha && confirmUpperAlpha && confirmNumber) {
+            parameters = lowerAlpha + upperAlpha + number
+        }
+        else if (confirmLowerAlpha && confirmUpperAlpha && confirmSpecial) {
+            parameters = lowerAlpha + upperAlpha + special
+        }
+        else if (confirmUpperAlpha && confirmNumber && confirmSpecial) {
+            parameters = upperAlpha + number + special
+        }
+        else if (confirmSpecial && confirmNumber && confirmLowerAlpha) {
+            parameters = special + number + lowerAlpha
+        }
+        // 2 options
+        else if (confirmLowerAlpha && confirmUpperAlpha) {
+            parameters = lowerAlpha + upperAlpha
+        }
+        else if (confirmLowerAlpha && confirmNumber) {
+            parameters = lowerAlpha + number
+        }
+        else if (confirmLowerAlpha && confirmSpecial) {
+            parameters = lowerAlpha + special
+        }
+        else if (confirmUpperAlpha && confirmNumber) {
+            parameters = upperAlpha && number
+        }
+        else if (confirmUpperAlpha && confirmSpecial) {
+            parameters = upperAlpha + special
+        }
+        else if (confirmNumber && confirmSpecial) {
+            parameters = number + special
+        }
+        // only 1 selection
+        else if (confirmLowerAlpha) {
+            parameters = lowerAlpha
+        }
+        else if (confirmUpperAlpha) {
+            parameters = upperAlpha
+        }
+        else if (confirmNumber) {
+            parameters = number
+        }
+        else if (confirmSpecial) {
+            parameters = special
+        }
+    };
+};
+
+// creating empty password variable to store the generated password in
+var password = [];
+
+
 
 
 
